7aa46c6fc0e6004df03cd3d021bcf348f376325f
Jesse Shawl <jesse@jshawl.com>
2015-03-15 14:03:10 +0000

    make prod

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get('/:id', routes.show)
 app.get('/:id/:filename', routes.showFile)
 app.post('/update', routes.updateFile)
 
-var server = app.listen(3000, function () {
+var server = app.listen(4000, function () {
     var host = server.address().address
     var port = server.address().port
     console.log('Listening at http://%s:%s', host, port)
