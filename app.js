0575fcfb112f07b456f72d565076aa7fb8096352
duncangraham <duncan@mapbox.com>
2015-03-15 23:59:01 -0500

    gulp + sass

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var sesh = require('express-session')
-var sass = require('node-sass-middleware')
 var routes = require('./routes')
 var config = require('./config')
 var path = require('path')
@@ -17,20 +16,13 @@ app.use(sesh({
   saveUninitialized: true,
   resave: true
 }))
+
 app.use(function(req,res,next){
   res.locals.session = req.session;
   res.locals.callback_uri = config.callback_uri
   res.locals.client_id = config.client_id
   next();
 });
-
-app.use(sass({
-    src: path.join(__dirname, 'sass'),
-    dest: path.join(__dirname, 'public'),
-    debug: true,
-    outputStyle: 'compressed',
-    prefix:  '/prefix'
-}));
 
 app.use(express.static(__dirname + '/public'));
 
