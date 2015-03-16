f944027d7a641c0f57d5922e416ce422cee30b02
duncangraham <duncan@mapbox.com>
2015-03-15 20:33:36 -0500

    sass

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express')
-var app = express()
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var sesh = require('express-session')
+var sass = require('node-sass-middleware')
 var routes = require('./routes')
 var config = require('./config')
-var exphbs = require('express-handlebars');
+var path = require('path')
+var exphbs = require('express-handlebars')
+
+var app = express()
 
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,6 +19,15 @@ app.use(function(req,res,next){
   res.locals.client_id = config.client_id
   next();
 });
+
+app.use(sass({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'
+}));
+
 app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -29,11 +41,5 @@ app.get('/:id', routes.show)
 app.get('/:id/:filename', routes.showFile)
 app.post('/update', routes.updateFile)
 app.post('/fork', routes.fork)
-
-var server = app.listen(4000, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log('Listening at http://%s:%s', host, port)
-})
 
 module.exports = app;
