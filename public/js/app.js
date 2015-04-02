e03f77aee88ba05640d5552f7453322844f5e994
Jesse Shawl <jesse@jshawl.com>
2015-04-02 12:12:50 -0400

    wip/ add ajax form submit

--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,16 @@ function autosize(){
     el.style.cssText = 'height:' + parseInt(el.scrollHeight + 35 )+ 'px';
   },0);
 }
+
+$('[data-submit]').on('click', function( event ){
+  event.preventDefault()
+  var $submittee = $($(this).data('submit'))
+  $.ajax({
+    url: $submittee.attr('action'),
+    method: $submittee.attr('method'),
+    data: $submittee.serialize(),
+    success: function( response ){
+      console.log( response )
+    }
+  })
+})
