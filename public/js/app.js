dda84f4eac4bf72dd96cfa2a7849438ddc48bdaa
Jesse Shawl <jesse@jshawl.com>
2015-04-03 19:30:57 -0400

    refactor save functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,21 @@
+$('.js-hide').hide()
+var Gistalt = Object.create( new ActiveStorage("Gistalt") )
 var cm = CodeMirror.fromTextArea(document.getElementById('content'), {
     mode: 'gfm',
     lineWrapping: true,
     theme: "default"
 });
-
-
 cm.on("change", function( cm ){
   $("[data-saved]").attr('data-saved', cm.getValue() == g.content )
 })
-
 var textarea = document.querySelector('.CodeMirror');
-
-window.onload = autosize;
-textarea.addEventListener('keydown', autosize);
-Gistalt = Object.create( new ActiveStorage("Gistalt") )
+var autosize = function autosize(){
+  var el = textarea;
+  setTimeout(function(){
+    el.style.cssText = 'padding:0';
+    el.style.cssText = 'height:' + parseInt(el.scrollHeight + 35 )+ 'px';
+  },0);
+}
 var g = Gistalt.findBy({
   gist_id: $("[name='id']").val()
 })
@@ -22,22 +24,20 @@ if( !g ){
     gist_id: $("[name='id']").val(),
     content: $('#content').html()
   })
-}
-function autosize(){
-  var el = textarea;
-  setTimeout(function(){
-    el.style.cssText = 'padding:0';
-    el.style.cssText = 'height:' + parseInt(el.scrollHeight + 35 )+ 'px';
-  },0);
+}else{
+  g.content = $('#content').html()
 }
 
-$('.js-hide').hide()
+autosize()
+textarea.addEventListener('keydown', autosize);
+
 $('[data-submit]').on('click', function( event ){
   $(this).attr('data-saved', true )
   event.preventDefault()
   var $submittee = $($(this).data('submit'))
   cm.save() // update textarea
   g.content = cm.getValue()
+  g.save()
   $.ajax({
     url: $submittee.attr('action') + '/json',
     method: $submittee.attr('method'),
