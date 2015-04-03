769fb248ada5302645cd0e6903ae842d74019784
Jesse Shawl <jesse@jshawl.com>
2015-04-03 09:03:55 -0400

    wip save state

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,27 @@ var cm = CodeMirror.fromTextArea(document.getElementById('content'), {
     theme: "default"
 });
 
+var isDirty = function(){
+  $("[data-saved]").attr('data-saved', cm.getValue() == g.content )
+}
+
+cm.on("change", function( cm ){
+})
+
 var textarea = document.querySelector('.CodeMirror');
 
 window.onload = autosize;
 textarea.addEventListener('keydown', autosize);
-             
+Gistalt = Object.create( new ActiveStorage("Gistalt") )
+var g = Gistalt.findBy({
+  gist_id: $("[name='id']").val()
+})
+if( !g ){
+  Gistalt.create({
+    gist_id: $("[name='id']").val(),
+    content: $('#content').html()
+  })
+}
 function autosize(){
   var el = textarea;
   setTimeout(function(){
@@ -22,6 +38,7 @@ $('[data-submit]').on('click', function( event ){
   event.preventDefault()
   var $submittee = $($(this).data('submit'))
   cm.save() // update textarea
+  g.content = cm.getValue()
   $.ajax({
     url: $submittee.attr('action') + '/json',
     method: $submittee.attr('method'),
