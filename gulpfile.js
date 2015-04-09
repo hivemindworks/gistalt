7c42f0cfc7215879e81daf25696d7d54bc8eec10
Jesse Shawl <jesse@jshawl.com>
2015-04-09 07:05:24 -0400

    wip fingerprint assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
+    path = require('path'),
+    fs = require('fs'),
+    rev = require('gulp-rev'),
+    concat = require('gulp-concat')
     sass = require('gulp-sass'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon')
 
 gulp.task('sass', function () {
     return gulp.src('./sass/style.scss')
@@ -8,6 +12,23 @@ gulp.task('sass', function () {
             .pipe(gulp.dest('public/css'));
 });
 
+gulp.task('rev', function(){
+  return gulp.src(['./public/dist/app.js', './dist/style.css'], {base:path.join(process.cwd(),'/' )})
+    .pipe(rev())
+    .pipe(gulp.dest(path.join(process.cwd(), '')))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest(path.join(process.cwd(), '/public/dist')))
+})
+
+gulp.task('concat', function(){
+return gulp.src('./public/js/*.js')
+    .pipe(concat('app.js'))
+        .pipe(gulp.dest('./public/dist/'));
+})
+
+gulp.task('build', function(){
+  gulp.run('concat')
+})
 
 // The default task (called when you run `gulp`)
 gulp.task('default', function() {
